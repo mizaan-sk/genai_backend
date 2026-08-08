0548d30f38814dbd55b276194b7579296c204b71
mizaan-sk <mizaandev@gmail.com>
2026-08-08 11:39:26 +0530

    Register Router Fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ const mongoose= require("mongoose");
 const userSchema = new mongoose.Schema({
     username:{
         type:String,
-        // unique:[true,"Username already taken"],
+        unique:[true,"Username already taken"],
         required : true
     },
     email:{
